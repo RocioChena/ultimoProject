d9cd4ce18ee5e88cea9a2228def3bd7cfa2968a5
RocioChena <rociochena17@gmail.com>
2024-08-04 22:45:02 -0400

    Update TodosIssues.vue

--- src/views/TodosIssues.vue
+++ src/views/TodosIssues.vue
@@ -6,34 +6,37 @@
       <el-input placeholder="Agregar una nueva tarea" v-model="todo"></el-input>
     </form>
 
-    <!-- Mostrar la lista de todos -->
     <el-row :gutter="12">
-      <el-col :span="12" v-for="(todo, index) in todos" :key="index">
-        <!-- Usar el componente TodoItem para cada todo -->
-        <TodoItem :todo="todo" @removeTodo="removeTodo(index)" :isIssue="false" />
-      </el-col>
-    </el-row>
-
-    <!-- Mostrar la lista de issues de GitHub -->
-    <el-row :gutter="12">
-      <el-col :span="12" v-for="(issue, index) in issues" :key="issue.id">
-        
-        <TodoItem :todo="issue" @closeIssue="closeIssue(index)" @remove="removeTodo(index)
-        " :isIssue="true" />
-      </el-col>
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :todo="todo"
+        :index="index"
+        @remove-todo="removeTodo"
+      />
+      <TodoItem
+        v-for="(issue, index) in issues"
+        :key="issue.id"
+        :todo="issue"
+        :index="index"
+        @remove-todo="removeTodo"
+        @close-issue="closeIssue"
+      />
     </el-row>
   </div>
 </template>
 
 <script>
+console.log("https://api.github.com/repos/RocioChena/ultima");
+console.log('Token:', process.env.VUE_APP_GITHUB_TOKEN);
 import axios from 'axios';
 import TodoItem from './TodoItem.vue'; // Ajusta la ruta según tu estructura de archivos
 
 const client = axios.create({
-  baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`,
+  baseURL: "https://api.github.com/repos/RocioChena/ultima",
   headers: {
-    'Authorization': `token ${process.env.VUE_APP_GITHUB_TOKEN}`,
-    'Accept': 'application/vnd.github.v3+json',
+    Authorization: `token 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`,
+    Accept: 'application/vnd.github.v3+json',
     'Content-Type': 'application/json',
   },
 });
@@ -53,7 +56,7 @@ export default {
   methods: {
     addTodo() {
       if (this.todo) {
-        this.todos.push({ title: this.todo, id: Date.now() });
+        this.todos.push({ title: this.todo, id: Date.now(), type: 'task' });
         this.todo = '';
       }
     },
@@ -62,21 +65,28 @@ export default {
     },
     closeIssue(index) {
       const target = this.issues[index];
-      client.patch(`/issues/${target.number}`, {
-        state: 'closed',
-      })
-      .then(() => {
-        this.issues.splice(index, 1);
-      })
-      .catch(error => {
-        console.error('Error al cerrar el issue:', error);
-      });
+      console.log('Closing issue with number:', target.number); // Añadir log para depuración
+      client
+        .patch(`/issues/${target.number}`, {
+          state: 'closed',
+        })
+        .then((response) => {
+          console.log('Issue closed response:', response); // Añadir log para verificar la respuesta
+          this.issues.splice(index, 1);
+        })
+        .catch((error) => {
+          console.error('Error al cerrar el issue:', error);
+        });
     },
     getIssues() {
-      client.get('issues')
+      client
+        .get('/issues')
         .then((res) => {
-          this.issues = res.data;
-        })
+      this.issues = res.data.map(issue => ({
+        ...issue,
+        type: 'issue'
+      }));
+    })
         .catch((error) => {
           console.error('Error al obtener los issues:', error);
         });
